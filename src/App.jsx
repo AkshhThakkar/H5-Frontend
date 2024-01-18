51d3fe7b524909f2ff3bee09a492c6f6994f9890
Aksh <aksht455@gmail.com>
2024-01-18 15:02:23 +0530

    routing changes

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,10 @@ import "./App.css";
 import Header from "./Components/Header";
 import Sidebar from "./Components/Sidebar";
 import Home from "./Components/Homepage";
+import HomePage from "./Components/Homepage";
+import Dashboard from "./Components/Frontend Layouts/Dashboard";
+import Categories from "./Components/Frontend Layouts/Categories";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 function App() {
   const [openSidebarToggle, setOpenSidebarToggle] = useState(false);
@@ -14,11 +18,19 @@ function App() {
   return (
     <div className="grid-container">
       <Header OpenSidebasr={OpenSidebar} />
-      <Sidebar
-        openSidebarToggle={openSidebarToggle}
-        OpenSidebar={OpenSidebar}
-      />
-      <Home />
+      <BrowserRouter>
+        <Routes>
+          <Route exact path="/" element={<HomePage />} />
+          <Route exact path="/dashboard" element={<Dashboard />} />
+          <Route path="/categories" element={<Categories />} />
+        </Routes>
+        {
+          <Sidebar
+            openSidebarToggle={openSidebarToggle}
+            OpenSidebar={OpenSidebar}
+          />
+        }
+      </BrowserRouter>
     </div>
   );
 }
