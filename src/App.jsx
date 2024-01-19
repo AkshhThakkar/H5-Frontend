4b9262b0b3ac8f10ad1874267109a9e9b032f35e
Aksh <aksht455@gmail.com>
2024-01-19 10:59:58 +0530

    Routing all components

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,11 @@ import Home from "./Components/Homepage";
 import HomePage from "./Components/Homepage";
 import Dashboard from "./Components/Frontend Layouts/Dashboard";
 import Categories from "./Components/Frontend Layouts/Categories";
+import Customers from "./Components/Frontend Layouts/Customers";
+import Inventory from "./Components/Frontend Layouts/Inventory";
+import Products from "./Components/Frontend Layouts/Products";
+import Reports from "./Components/Frontend Layouts/Reports";
+import Settings from "./Components/Frontend Layouts/Settings";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 function App() {
@@ -23,6 +28,11 @@ function App() {
           <Route exact path="/" element={<HomePage />} />
           <Route exact path="/dashboard" element={<Dashboard />} />
           <Route path="/categories" element={<Categories />} />
+          <Route path="/categories" element={<Customers />} />
+          <Route path="/categories" element={<Inventory />} />
+          <Route path="/categories" element={<Products />} />
+          <Route path="/categories" element={<Reports />} />
+          <Route path="/categories" element={<Settings />} />
         </Routes>
         {
           <Sidebar
