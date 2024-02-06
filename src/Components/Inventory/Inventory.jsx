7039243b045ea574f927f55a45936035f96a453d
Aksh <aksht455@gmail.com>
2024-02-06 13:56:48 +0530

    Inventory jsx

--- src/Components/Inventory/Inventory.jsx
+++ src/Components/Inventory/Inventory.jsx
@@ -1,7 +1,25 @@
 import React from "react";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import "./Inventory.css";
 
-const Inventory = () => {
-  return <div>Inventory</div>;
-};
+function Inventory() {
+  const [myData, setMyData] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("http://192.168.3.237:5760/api/products/getProducts")
+      .then((res) => {
+        setMyData(res.data);
+        console.log("Data received:", res.data);
+      })
+      .catch((error) => {
+        console.error("Error fetching data:", error);
+      });
+  }, []);
+
+  console.log("myData:", myData);
+  return console.log("MYDATA", myData);
+}
 
 export default Inventory;
