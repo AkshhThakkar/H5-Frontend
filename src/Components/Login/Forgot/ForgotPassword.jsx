00561a8fd56a9dee47f1b9f62710a72688a8f23e
Aksh <aksht455@gmail.com>
2024-02-20 11:05:04 +0530

    reset link commit

--- src/Components/Login/Forgot/ForgotPassword.jsx
+++ src/Components/Login/Forgot/ForgotPassword.jsx
@@ -1,24 +1,23 @@
 import React, { useState } from "react";
-import { Field, Formik } from "formik";
 import {
+  Grid,
   Paper,
   Avatar,
+  Typography,
   TextField,
   Button,
-  Typography,
-  Link,
   CircularProgress,
 } from "@material-ui/core";
 import { EmailRounded } from "@material-ui/icons";
-import { Grid } from "@material-ui/core";
-import LockOutlinedIcon from "@material-ui/icons/LockOutlined";
+import { Formik, Field, Form, ErrorMessage } from "formik";
+import * as Yup from "yup";
 import axios from "axios";
-import { useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 const ForgotPassword = () => {
   const paperStyle = {
     padding: 20,
-    height: "60vh",
+    height: "50vh",
     width: 300,
     margin: "0 auto",
     backgroundColor: "#f0f3f5",
@@ -26,78 +25,96 @@ const ForgotPassword = () => {
     borderRadius: "8px",
   };
   const avatarStyle = { backgroundColor: "#3498db", color: "#ffffff" };
-  const btnstyle = { margin: "15px 0" };
-  const marginTop = { marginTop: 60 };
-  const [email, setEmail] = useState("");
+  const btnContainerStyle = { textAlign: "center", marginTop: 20 }; // Added marginTop
+  const marginTop = { marginTop: 15 };
+
   const [message, setMessage] = useState("");
   const [loading, setLoading] = useState(false);
-  const navigate = useNavigate();
 
-  const handleForgotPassword = async () => {
+  const initialValues = {
+    email: "",
+  };
+
+  const validationSchema = Yup.object().shape({
+    email: Yup.string().email("Invalid email").required("Email is required"),
+  });
+
+  const handleForgotPassword = async (values, { setSubmitting }) => {
     setLoading(true);
     try {
       const response = await axios.post(
         "http://192.168.3.237:5760/api/pass/forgot-password",
-        { email }
+        { email: values.email }
       );
       setMessage(response.data.message);
-      setTimeout(() => {
-        setLoading(false);
-        navigate("/reset");
-      }, 3000); // 3 seconds delay
     } catch (error) {
-      setLoading(false);
-      setMessage(error.response.data.message);
+      console.error("Error:", error);
+      setMessage("An error occurred. Please try again later.");
     }
+    setLoading(false);
+    setSubmitting(false);
   };
 
   return (
     <Grid
       container
       alignItems="center"
+      justify="center"
       style={{ height: "100vh", width: "100vw", backgroundColor: "#1d2634" }}>
       <Paper style={paperStyle}>
         <Grid container direction="column" alignItems="center" spacing={2}>
           <Avatar style={avatarStyle}>
-            <LockOutlinedIcon />
+            <EmailRounded />
           </Avatar>
-          <h2>Forgot password</h2>
+          <Typography variant="h5">Forgot Password</Typography>
         </Grid>
-        <p>We will send you a recovery code to your email</p>
-        <Formik>
-          <Field
-            as={TextField}
-            fullWidth
-            name="email"
-            onChange={(e) => setEmail(e.target.value)}
-            autoComplete="on"
-            label="Email"
-            type="email"
-            placeholder="Enter your email"
-            value={email}
-            InputProps={{
-              startAdornment: <EmailRounded style={{ marginRight: "10px" }} />,
-            }}
-          />
-        </Formik>
-        <Button
-          type="submit"
-          color="primary"
-          variant="contained"
-          style={btnstyle}
-          fullWidth
-          onClick={handleForgotPassword}
-          disabled={loading}>
-          {loading ? (
-            <CircularProgress size={24} color="white" />
-          ) : (
-            "Reset Password"
+        <p>We will send you a password recovery link to your email</p>
+        <Formik
+          initialValues={initialValues}
+          validationSchema={validationSchema}
+          onSubmit={handleForgotPassword}>
+          {(props) => (
+            <Form>
+              <div style={marginTop}>
+                <Field
+                  as={TextField}
+                  fullWidth
+                  name="email"
+                  autoComplete="on"
+                  label="Email"
+                  placeholder="Enter your email"
+                  error={props.touched.email && !!props.errors.email}
+                  helperText={<ErrorMessage name="email" />}
+                  InputProps={{
+                    startAdornment: (
+                      <EmailRounded style={{ marginRight: "10px" }} />
+                    ),
+                  }}
+                />
+              </div>
+              <div style={btnContainerStyle}>
+                <Button
+                  type="submit"
+                  variant="contained"
+                  disabled={props.isSubmitting}
+                  color="primary"
+                  style={{ width: "100%" }}>
+                  {props.isSubmitting ? (
+                    <CircularProgress size={24} color="primary" />
+                  ) : (
+                    "Reset Password"
+                  )}
+                </Button>
+              </div>
+            </Form>
           )}
-        </Button>
-        {message && <Typography>{message}</Typography>}
+        </Formik>
+        <div style={marginTop}>
+          {message && <Typography>{message}</Typography>}
+        </div>
         <div style={marginTop}>
           <Typography>
-            <Link href="/">Back </Link>
+            <Link href="/"> Login? </Link>
           </Typography>
         </div>
       </Paper>
